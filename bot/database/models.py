9e1125900426b488cacd5b0c1337683ec8f6f2e6
Welzewool <blackenedima@yandex.ru>
2025-03-20 16:57:35 +0300

    исправлен импорт

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from database.database import Base
+from bot.database.database import Base
 
 
 class User(Base):
